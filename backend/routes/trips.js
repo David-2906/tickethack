edc30240626df0d3f4db677582b8d5ca4bfbaa75
leabonnenfant <leabonnenfant@gmail.com>
2024-04-16 15:38:25 +0200

    route trips pour rechercher départ et arrivé

--- backend/routes/trips.js
+++ backend/routes/trips.js
@@ -2,10 +2,23 @@ var express = require("express")
 var router = express.Router()
 
 const Trip = require("../models/trips")
-const Cart = require("../models/carts") 
-const Bookings = require("../models/bookings")  
+// const Cart = require("../models/carts") 
+// const Bookings = require("../models/bookings")  
 
 // const { checkBody } = require("../modules/checkBody") 
 
+// Rechercher un nouveau trajet dans départ et arrivé 
+router.get("/trips/:departure/:arrival", (req, res) => {
+    Trip.findOne({
+      departure: { $regex: new RegExp(req.params.departure, "i") },
+      arrival: { $regex: new RegExp(req.params.arrival, "i") },
+    }).then(data => {
+      if (data) {
+        res.json({ result: true, Trip: data });
+      } else {
+        res.json({ result: false, error: "Trip not found" });
+      }
+    })
+  });
 
 module.exports = router
