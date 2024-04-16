4f994f51fb8339717d1e1a77e80103a7f63196b7
leabonnenfant <leabonnenfant@gmail.com>
2024-04-16 14:17:03 +0200

    importation const cart et const bookings dans trip.js

--- backend/routes/trips.js
+++ backend/routes/trips.js
@@ -1,7 +1,9 @@
 var express = require("express")
 var router = express.Router()
 
-const Trip = require("../models/trips") 
+const Trip = require("../models/trips")
+const Cart = require("../models/carts") 
+const Bookings = require("../models/bookings")  
 
 // const { checkBody } = require("../modules/checkBody") 
 
