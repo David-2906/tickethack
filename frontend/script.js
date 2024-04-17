8d0a5042ea64e1970f555090b8bc62af3dd08ca7
David <david.pires2906@gmail.com>
2024-04-17 11:55:42 +0200

    premiere partie script

--- frontend/script.js
+++ frontend/script.js
@@ -4,4 +4,27 @@ document.querySelector('#searchButton').addEventListener('click', function() {
     const date = document.querySelector('#day').value;
 
     fetch(`http://localhost:3000/trips/${departure}/${arrival}/${date}`)
+    .then((res)=>res.json())
+    .then((data)=>{ 
+        if(data.result === false){
+            document.querySelector('#tripList').innerHTML = ``;
+            document.querySelector('#tripList').innerHTML += `
+        <img src="./images/notfound.png" alt="Une image de loupe" id="trainImg">
+        <hr>
+        <p>No trip found</p>`
+    } else {
+        document.querySelector('#tripList').innerHTML = ``;
+        const myTrip = data.Trip;
+        const pattern = /(\d{2}:\d{2})/;
+        for(let trip of myTrip){
+            document.querySelector('#tripList').innerHTML += `
+            <div id="listTrip">
+            <p>${trip.departure} > ${trip.arrival} ${trip.date.match(pattern)[0]} ${trip.price} € </p>
+            <button class="bookButton">Book</button>
+            </div>`
+        }
+    }
+    })
+
 })
+
